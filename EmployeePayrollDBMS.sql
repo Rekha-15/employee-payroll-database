cb8f1e3634aceaee460db33516f543be2a62b7bc
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-02 22:40:33 -0400

    added : Created Employee Payroll Table : .

--- EmployeePayrollDBMS.sql
+++ EmployeePayrollDBMS.sql
@@ -1,4 +1,14 @@
-Create database payroll_service; #creating data base
-show databases; # displaying database
-use payroll_service;  
-SELECT DATABASE(); #selecting required data base
+create database payroll_service;
+show databases;
+use payroll_service;
+
+CREATE TABLE employee_payroll		# It is use to create a tablein the database
+     (
+     id       INT unsigned NOT NULL AUTO_INCREMENT,
+     name     VARCHAR (100) NOT NULL,
+     salary   Double NOT NULL,
+     start    Date NOT NULL,
+     PRIMARY KEY  (id)			# Primary key is use to Create a id as a unique.
+     );
+     DESCRIBE employee_payroll;		# It is use to display the table.
+     Select*from employee_payroll;
